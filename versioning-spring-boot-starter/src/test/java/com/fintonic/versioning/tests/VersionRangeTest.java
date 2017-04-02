af4cdc99d32c3af906976dc573f109c98d8c2835
Rafael Ríos <ganchix@gmail.com>
2017-04-02 20:41:38 +0200

    Added VersionRange and Version tests

---
+++ versioning-spring-boot-starter/src/test/java/com/fintonic/versioning/tests/VersionRangeTest.java
@@ -0,0 +1,25 @@
+package com.fintonic.versioning.tests;
+
+import com.fintonic.versioning.domain.VersionRange;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by Rafael Ríos on 2/04/17.
+ */
+@RunWith(JUnit4.class)
+public class VersionRangeTest {
+
+    @Test(expected = Exception.class)
+    public void testEmptyFromVersion_ShouldReturnException() {
+        new VersionRange(null, "1.0");
+    }
+
+    @Test(expected = Exception.class)
+    public void testFromLessThanTo_ShouldReturnException() {
+        new VersionRange("2.0", "1.0");
+
+    }
+
+}
